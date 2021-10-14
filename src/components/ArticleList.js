8f6eff49493107573e37ee50ebf5b1aa5c74ad60
garrettdick <garrettd0511@gmail.com>
2021-10-14 18:34:10 -0500

    commit tree 468b10d3248f1935bddaedc94bfcca1d2ca5dbab

---
+++ src/components/ArticleList.js
@@ -0,0 +1,11 @@
+import Article from "./Article"
+
+function ArticleList({ posts }){
+
+    const renderArticles = posts.map(({id, title, date, preview, minutes}) => <Article key={id} title={title} date={date} preview={preview} minutes={minutes} /> )
+    return (
+        <main>{renderArticles}</main>
+    )
+}
+
+export default ArticleList; 
